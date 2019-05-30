b4ebefe909454ce8e65ff6e87e36bcb087e579c1
dunglamtuan <dunglamtuan@gmail.com>
2019-05-30 22:24:08 +0200

    Initialize BE backbone, #1

---
+++ src/main/java/sk/garwan/animal/shop/AnimalEshopApplication.java
@@ -0,0 +1,13 @@
+package sk.garwan.animal.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalEshopApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AnimalEshopApplication.class, args);
+  }
+
+}
