bde042df6e64b234681e9b1bfa0048f2c62244be
dunglamtuan <dunglamtuan@gmail.com>
2019-06-03 22:32:45 +0200

    Fix some integration tests (#22)

--- src/test/java/sk/garwan/animal/shop/service/DefaultUserServiceTest.java
+++ src/test/java/sk/garwan/animal/shop/service/DefaultUserServiceTest.java
@@ -10,9 +10,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import sk.garwan.animal.shop.model.User;
 import sk.garwan.animal.shop.repository.UserRepository;
+import sk.garwan.animal.shop.security.jwt.JwtTokenProvider;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DefaultUserServiceTest {
@@ -23,17 +26,24 @@ public class DefaultUserServiceTest {
   @InjectMocks
   private DefaultUserService userService;
 
+  @Spy
+  private PasswordEncoder passwordEncoder;
+
+  @Mock
+  private JwtTokenProvider jwtTokenProvider;
+
   @Test
   public void registrationTest() {
     //setup
     String testPassword = "password";
     User user = User.builder().username("username").email("email1").password(testPassword).build();
     when(userRepository.save(any())).thenReturn(user);
-    when(userRepository.findByUsername(anyString())).thenReturn(null);
+    when(userRepository.findByUsername(anyString())).thenReturn(Optional.empty());
+    when(jwtTokenProvider.createToken(anyString(), any())).thenReturn("token");
 
     //execute
-    Optional<String> token = userService
-        .registerNewUser(User.builder().password("any").build());
+    Optional<String> token = userService.registerNewUser(
+        User.builder().password("any").username("username").isAdmin(false).email("email").build());
 
     //verify
     assertThat(token).isPresent();
